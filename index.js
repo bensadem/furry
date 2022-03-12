a5891968e8ca824fd80ddfec182ea1d3d534e930
bensadem <80596375+bensadem@users.noreply.github.com>
2022-03-11 16:05:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -534,7 +534,7 @@ channel.send(embed)
 
 
 client.on('message' , saud=> {
-if(saud.content === `<@!${client.user.id}>>`){
+if(saud.content === `<@${client.user.id}>`
 saud.channel.send(`**My prefix is +**`)
 }
 });
@@ -571,30 +571,6 @@ let devs = ["712379964498116670"];
   }
 });
 
-client.on("message", async ncr => {
-    if (ncr.content.startsWith(prefix + "ping")) {
-    var states = "🟢 Excellent";
-    var states2 = "🟢 Excellent";
 
-    var msg = `${Date.now() - ncr.createdTimestamp}`;
-    var api = `${Math.round(client.ws.ping)}`;
-    if (Number(msg) > 70) states = "🟢 Good";
-    if (Number(msg) > 170) states = "🟡 Not Bad";
-    if (Number(msg) > 350) states = "🔴 Soo Bad";
-    if (Number(api) > 70) states2 = "🟢 Good";
-    if (Number(api) > 170) states2 = "🟡 Not Bad";
-    if (Number(api) > 350) states2 = "🔴 Soo Bad";
-    if (ncr.author.bot) return;
-    ncr.channel.send(
-      new MessageEmbed()
-        .setColor("00e8ff")
-        .setAuthor(ncr.author.username, ncr.author.avatarURL())
-        .addField("**Time Taken:**", msg + " ms 📶 | " + states, true)
-        .addField("**WebSocket:**", api + " ms 📶 | " + states2, true)
-        .setTimestamp()
-        .setFooter(`Request By ${ncr.author.tag}`)
-    ); // All Copyrights Go's To </> NAAR Studio: https://discord.gg/YJ6mUdgTscs
-    }
-});
 
 client.login(process.env.token); 
